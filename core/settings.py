458361ae78ace2b0ff7bd4df3d00934990139c39
Evan Allrich <evan@unguku.com>
2014-03-13 22:13:50 -0500

    Add whisper settings

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,20 @@
+import os
+
+# From metricdock/core/settings.py to metricdock/
+cwd = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(cwd), os.pardir))
+
+
+# logging
+# -------
 logging = {
     'format': '[%(levelname)s] %(message)s',
     'level':  'DEBUG',
 }
 
+
+# redis
+# -----
 redis_server = {
     'host': 'localhost',
     'port': 6379,
@@ -11,3 +23,11 @@ redis_server = {
 
 redis_latest = 'metrics:latest'
 redis_latest_bound = 600 # seconds
+
+
+# whisper
+# -------
+whisper_path = os.path.join(PROJECT_ROOT, "whisper")
+whisper_archives = "10s:24h,1m:7d,5m:30d,1h:2y"
+whisper_xfilesfactor = 0.0
+whisper_aggregation = "average"
