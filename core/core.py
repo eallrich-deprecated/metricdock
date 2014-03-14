b7a642b6430d70ccec164dc0ceb3f04682d76faf
Evan Allrich <evan@unguku.com>
2014-03-13 21:21:25 -0500

    Partially implement /publish

--- core/core.py
+++ core/core.py
@@ -2,7 +2,7 @@ import json
 import logging
 import time
 
-from flask import Flask, g
+from flask import Flask, abort, g, request
 import redis
 
 from . import settings
@@ -42,12 +42,59 @@ def trim():
     discard_ts = int(time.time()) - settings.redis_latest_bound
     n = get_redis().zremrangebyscore(settings.redis_latest, '-inf', discard_ts)
     logger.info("Trimmed %d metrics from latest zset" % n)
-    return "%d" % n
+    return "%d\n" % n
 
-def save_to_redis(metrics):
+def save(metrics):
+    logger.info("Saving %d metrics" % len(metrics))
     now = int(time.time())
     r.zadd(settings.redis_latest, now, json.dumps(metrics))
 
 @app.route('/publish', methods=['POST'])
 def publish():
-    logger.debug("Publish called")
+    """Accepts metrics from clients
+    
+    Saves metrics to whisper for persistence and also stashes them in a queue
+    of latest metrics."""
+    if request.content_type != 'application/json':
+        abort(415) # Unsupported media type
+    
+    data = request.get_json()
+    
+    try:
+        # List or dictionary?
+        _ = data[0]
+    except KeyError:
+        # It's a dictionary, make it a list for consistency
+        data = [data,]
+    
+    metrics = []
+    
+    # Make sure the syntax is as expected. If not, return 400 Bad Syntax
+    for document in data:
+        clean = {}
+        
+        # Make sure all the keys exist
+        for key in ('metric', 'value', 'timestamp',):
+            try:
+                clean[key] = document[key]
+            except KeyError:
+                return "Missing required key '%s'\n" % key, 400
+        
+        # Float-able value?
+        try:
+            clean['value'] = float(clean['value'])
+        except ValueError:
+            return "'value' must be a float\n" % clean['value'], 400
+        
+        # Int-able timestamp?
+        try:
+            clean['timestamp'] = int(clean['timestamp'])
+        except ValueError:
+            return "'timestamp' must be an int\n" % clean['timestamp'], 400
+        
+        metrics.append(clean)
+    
+    save(metrics)
+    
+    # Created
+    return "Saved %d metrics\n" % len(metrics), 201
