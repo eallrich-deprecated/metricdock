01d999403c7ccd3b1ec9e953d8f954574a24e353
Evan Allrich <evan@unguku.com>
2014-03-16 14:00:09 -0500

    Test leaving the leading '/' in place

--- core/core.py
+++ core/core.py
@@ -35,7 +35,7 @@ def find_whispers():
         return whispers
     
     # We're going to remove this prefix from results
-    prefix = os.path.join(settings.whisper_path, '/')
+    prefix = settings.whisper_path
     
     for root, _, files in os.walk(settings.whisper_path):
         root = root.replace(prefix, '')
